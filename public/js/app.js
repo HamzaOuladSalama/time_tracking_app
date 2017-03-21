05e9bd5f8f98d8b335a160ec849d2f1f81dac631
Yousuf Nejati <yousufnejati@gmail.com>
2017-03-20 23:29:58 -0700

    Add EditableTimer component and reformat code

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,12 @@ class TimersDashboard extends React.Component {
   render() {
     return (
       <div className='ui three column centered grid'>
-  <div className='column'>
-    <EditableTimerList />
-    <ToggleableTimerForm 
-      isOpen={true}
-    />
-  </div>
+        <div className='column'>
+          <EditableTimerList />
+          <ToggleableTimerForm 
+            isOpen={true}
+          />
+        </div>
       </div>
     );
   }
@@ -20,14 +20,35 @@ class EditableTimerList extends React.Component {
     return (
       <div id='timers'>
         <EditableTimer
-    title='Learn React'
-    project='Web Domination'
-    elapsed='8986300'
-    runningSince={null}
-    editFormOpen={true}
-  />
+          title='Learn React'
+          project='Web Domination'
+          elapsed='8986300'
+          runningSince={null}
+          editFormOpen={true}
+        />
       </div>  
     );
   }
 }
 
+class EditableTimer extends React.Component {
+  render() {
+    if (this.props.editFormOpen) {
+      return (
+        <TimerForm
+          title={this.props.title}
+          project={this.props.project}
+        />
+      );
+    } else {
+      return (
+        <Timer
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      );
+    }
+  }
+}
