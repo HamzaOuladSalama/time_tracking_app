165e6b5c68096fd7c23f01ad46ffa730b2d666d8
Yousuf Nejati <yousufnejati@gmail.com>
2017-03-22 11:36:48 -0700

    Add event handlers to EditableTimer component and both possible child components TimerForm and Timer

--- public/js/app.js
+++ public/js/app.js
@@ -80,13 +80,37 @@ class EditableTimer extends React.Component {
     editFormOpen: false,
   };
 
+  handleEditClick = () => {
+    this.openForm();
+  };
+
+  handleFormClose = () => {
+    this.closeForm();
+  };
+
+  handleSubmit = (timer) => {
+    this.props.onFormSubmit(timer);
+    this.closeForm();
+  };
+
+  closeForm = () => {
+    this.setState({ editFormOpen: false });
+  };
+
+  openForm = () => {
+    this.setState({ editFormOpen: true });
+  };
+
   render() {
     if (this.props.editFormOpen) {
       return (
         <TimerForm
 	  id = {this.props.id}
           title = {this.props.title}
-          project = {this.props.project} />
+          project = {this.props.project} 
+	  onFormSubmit={this.handleSubmit}
+	  onFormClose={this.handleFormClose}
+	  />
       );
     } else {
       return (
@@ -95,7 +119,8 @@ class EditableTimer extends React.Component {
           title={this.props.title}
           project={this.props.project}
           elapsed={this.props.elapsed}
-          runningSince={this.props.runningSince} 
+          runningSince={this.props.runningSince}
+	  onEditClick={this.handleEditClick} 
 	/>
       );
     }
